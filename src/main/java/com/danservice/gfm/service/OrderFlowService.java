a4cf2311ffd51d9c7199408726e51770cb1343c9
Daniele Parisi <neb.inf@gmail.com>
2024-03-14 16:05:48 +0100

    Remove fake delays (thread sleeps)

--- src/main/java/com/danservice/gfm/service/OrderFlowService.java
+++ src/main/java/com/danservice/gfm/service/OrderFlowService.java
@@ -4,8 +4,6 @@ import com.danservice.gfm.adapter.KafkaStreetOrderMapper;
 import com.danservice.gfm.adapter.inbound.kafka.clientorder.v1.dto.KafkaClientOrderDTO;
 import com.danservice.gfm.adapter.inbound.kafka.streetorderack.v1.dto.KafkaStreetOrderAckDTO;
 import com.danservice.gfm.adapter.outbound.kafka.streetorder.v1.KafkaProducer;
-import com.danservice.gfm.domain.OrderStatus;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +13,6 @@ import java.util.Random;
 import java.util.UUID;
 
 import static com.danservice.gfm.domain.OrderStatus.*;
-import static java.lang.Thread.sleep;
 
 @Slf4j
 @Service
@@ -33,8 +30,6 @@ public class OrderFlowService {
 
         var orderId = orderService
                 .save(clientOrderDTO, RECEIVED).getId();
-
-        sleep(RANDOM.nextInt(0, 500));
 
         kafkaProducer
                 .sendStreetOrder(kafkaStreetOrderMapper.map(clientOrderDTO));
