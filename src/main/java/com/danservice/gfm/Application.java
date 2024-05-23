821d601401f9bf46e4e57e569fe80424820d0622
Daniele Parisi <neb.inf@gmail.com>
2024-05-23 16:45:42 +0200

    Add configuration for native image build / push

--- src/main/java/com/danservice/gfm/Application.java
+++ src/main/java/com/danservice/gfm/Application.java
@@ -1,6 +1,8 @@
 package com.danservice.gfm;
 
+import com.danservice.gfm.adapter.outbound.kafka.streetorder.v1.dto.KafkaStreetOrderDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,6 +14,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
+@RegisterReflectionForBinding(KafkaStreetOrderDTO.class)
 public class Application {
 
     public static void main(String[] args) {
